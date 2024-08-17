0353811e5fabf1bdc397e020e966b3abd52e0c82
FrancisCidney <franciscidneyawuor@gmail.com>
2024-08-17 09:35:40 +0300

    added professional default login credentials

--- src/app/signin/sign-in-form.tsx
+++ src/app/signin/sign-in-form.tsx
@@ -11,8 +11,8 @@ import { routes } from '@/config/routes';
 import { loginSchema, LoginSchema } from '@/utils/validators/login.schema';
 
 const initialValues: LoginSchema = {
-  email: 'contractor@email.com',
-  password: 'contractor',
+  email: 'professional@email.com',
+  password: 'professional',
   rememberMe: true,
 };
 
